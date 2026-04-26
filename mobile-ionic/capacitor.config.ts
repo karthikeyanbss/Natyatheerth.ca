473396c7b3f9a1c093837ef29c4fb5bfc6c2e894
Bassen Metals Dev <admin@bassen-metals.com>
2026-04-26 12:39:38 -0300

    Added fix

--- mobile-ionic/capacitor.config.ts
+++ mobile-ionic/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ca.natyatheerth.app',
   appName: 'Natya Theerth',
-  webDir: 'www',
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   },
