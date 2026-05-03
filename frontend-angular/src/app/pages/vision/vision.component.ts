4df2178108d53e9fd6a7704b6fec266e744212f3
Bassen Metals Dev <admin@bassen-metals.com>
2026-05-03 12:44:50 -0300

    BRD-4: Remove white info box on guru page; update vision page title/gallery/lightbox; fix bharatanatyam level 05 heading layout

--- frontend-angular/src/app/pages/vision/vision.component.ts
+++ frontend-angular/src/app/pages/vision/vision.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-vision',
@@ -6,49 +6,20 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   templateUrl: './vision.component.html',
   styleUrls: ['./vision.component.scss']
 })
-export class VisionComponent implements OnInit, OnDestroy {
-  galleryImages: string[] = [
-    'assets/Gallery/gallery image 1.JPG',
-    'assets/Gallery/gallery image 2.JPG',
-    'assets/Gallery/gallery image 3.JPG',
-    'assets/Gallery/gallery image 4.JPG',
-    'assets/Gallery/gallery image 5.jpg',
-    'assets/Gallery/gallery image 6.jpg',
-    'assets/Gallery/gallery image 7.jpg',
+export class VisionComponent {
+  visionImages: string[] = [
     'assets/Gallery/gallery image 8.JPG',
     'assets/Gallery/gallery image 9.JPG',
     'assets/Gallery/gallery image 10JPG.JPG'
   ];
 
-  currentSlide = 0;
-  private autoplayTimer: ReturnType<typeof setInterval> | null = null;
+  selectedImage: string | null = null;
 
-  ngOnInit(): void {
-    this.startAutoplay();
+  openImage(src: string): void {
+    this.selectedImage = src;
   }
 
-  ngOnDestroy(): void {
-    this.stopAutoplay();
-  }
-
-  private startAutoplay(): void {
-    this.autoplayTimer = setInterval(() => this.nextSlide(), 3500);
-  }
-
-  private stopAutoplay(): void {
-    if (this.autoplayTimer) {
-      clearInterval(this.autoplayTimer);
-      this.autoplayTimer = null;
-    }
-  }
-
-  prevSlide(): void {
-    this.stopAutoplay();
-    this.currentSlide = (this.currentSlide - 1 + this.galleryImages.length) % this.galleryImages.length;
-    this.startAutoplay();
-  }
-
-  nextSlide(): void {
-    this.currentSlide = (this.currentSlide + 1) % this.galleryImages.length;
+  closeImage(): void {
+    this.selectedImage = null;
   }
 }
